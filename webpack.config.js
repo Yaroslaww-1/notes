cfd31fca8187c830689ef98eae8bc95a300bb8e2
Yaroslaww1 <yaroslaww1@gmail.com>
2020-09-13 21:56:09 +0300

    webpack + babel setup

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), 
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              configFile: "./babel.config.js",
+              cacheDirectory: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: 'index.html'
+  })],
+  mode: 'development',
+  devServer: {
+    publicPath: '/dist',
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+  },
+  devtool: "source-map"
+}

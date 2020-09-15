9a9d7a64055abad06a72ae390efb77e2e7d9c6d7
Yaroslaww1 <yaroslaww1@gmail.com>
2020-09-15 12:34:03 +0300

    fixed webpack build to different folders

--- webpack.config.js
+++ webpack.config.js
@@ -3,58 +3,99 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'), 
-    filename: 'bundle.js',
-    publicPath: '/dist/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              configFile: "./babel.config.js",
-              cacheDirectory: true
+module.exports = [
+  {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'), 
+      filename: 'bundle.js',
+      publicPath: '/dist/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                configFile: "./babel.config.js",
+                cacheDirectory: true
+              }
             }
-          }
-        ]
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: /\.(png|jpg)$/,
-        loader: 'url-loader'
-      }
-    ]
+          ]
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"]
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: 'url-loader'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html'
+      }),
+      new MiniCssExtractPlugin(),
+    ],
+    mode: 'development',
+    devServer: {
+      publicPath: '/dist',
+      contentBase: path.join(__dirname, 'dist'),
+      hot: true,
+    },
+    devtool: "source-map"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html'
-    }),
-    new MiniCssExtractPlugin(),
-    new FileManagerPlugin({
-      onEnd: {
-        copy: [{
-          source: './dist/*.*',
-          destination: './docs/',
-        }],
-      },
-    }),
-  ],
-  mode: 'development',
-  devServer: {
-    publicPath: '/dist',
-    contentBase: path.join(__dirname, 'dist'),
-    hot: true,
-  },
-  devtool: "source-map"
-}
+  {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'docs'), 
+      filename: 'bundle.js',
+      publicPath: '/docs/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                configFile: "./babel.config.js",
+                cacheDirectory: true
+              }
+            }
+          ]
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"]
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: 'url-loader'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html'
+      }),
+      new MiniCssExtractPlugin(),
+    ],
+    mode: 'development',
+    devServer: {
+      publicPath: '/dist',
+      contentBase: path.join(__dirname, 'dist'),
+      hot: true,
+    },
+    devtool: "source-map"
+  }
+];
